be0d767128aef4d715b40cd17a229e2d015c3ea9
thomas.dilts60@gmail.com <thomas.dilts60@gmail.com@cf19deb9-d20a-914d-38ac-59a7b5438621>
2011-05-08 12:48:38 +0000

    forgot to add some files to svn

---
+++ trunk/CrossConnect/EditBookmarks.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace CrossConnect
+{
+    public partial class EditBookmarks : PhoneApplicationPage
+    {
+        public EditBookmarks()
+        {
+            InitializeComponent();
+        }
+        private bool isInSelectionChanged=false;
+        private void SelectList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (isInSelectionChanged)
+            {
+                return;
+            }
+            isInSelectionChanged = true;
+            MessageBoxResult result= MessageBox.Show("Delete?","", MessageBoxButton.OKCancel);
+            if (result.Equals(MessageBoxResult.OK))
+            {
+                int index = (int)((TextBlock)e.AddedItems[0]).Tag;
+                App.placeMarkers.bookmarks.RemoveAt(index);
+                LoadList();
+            }
+            else
+            {
+                SelectList.SelectedItem = null;
+            }
+            isInSelectionChanged = false;
+        }
+
+        private void LoadList()
+        {
+            SelectList.Items.Clear();
+            List<string> allBookmarks = App.openWindows[0].state.source.makeListDisplayText(App.placeMarkers.bookmarks);
+            //the list is a reversed list from the original list. So we must mark it with the correct reversed index.
+            int j = allBookmarks.Count - 1;
+            for (int i = 0; i < allBookmarks.Count; i++)
+            {
+                TextBlock block = new TextBlock();
+                block.Text = allBookmarks[i].Replace("<p>", "").Replace("</p>", "");
+                block.Tag = j--;
+                block.TextWrapping = TextWrapping.Wrap;
+                SelectList.Items.Add(block);
+            }
+        }
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            LoadList();
+        }
+    }
+}
---
+++ trunk/CrossConnect/RemoveBibles.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace CrossConnect
+{
+    public partial class RemoveBibles : PhoneApplicationPage
+    {
+        public RemoveBibles()
+        {
+            InitializeComponent();
+        }
+
+        private bool isInSelectionChanged = false;
+        private void SelectList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (isInSelectionChanged)
+            {
+                return;
+            }
+            isInSelectionChanged = true;
+            if (App.installedBibles.installedBibles.Count == 1)
+            {
+                MessageBoxResult result = MessageBox.Show("You must always have at least one bible.");
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Delete?", "", MessageBoxButton.OKCancel);
+                if (result.Equals(MessageBoxResult.OK))
+                {
+                    string index = (string)((TextBlock)e.AddedItems[0]).Tag;
+                    foreach (var book in App.installedBibles.installedBibles)
+                    {
+                        if (book.Value.sbmd.Initials.Equals(index))
+                        {
+                            for (int i = App.openWindows.Count()-1; i >=0 ; i--)
+                            {
+                                if (App.openWindows[i].state.bibleToLoad.Equals(book.Value.sbmd.internalName))
+                                {
+                                    App.openWindows.RemoveAt(i);
+                                    App.mainWindow.ReDrawWindows();
+                                }
+                            }
+                            book.Value.RemoveBible();
+                            App.installedBibles.installedBibles.Remove(book.Key);
+                            App.installedBibles.save();
+                            break;
+                        }
+                    }
+                    LoadList();
+                }
+                else
+                {
+                    SelectList.SelectedItem = null;
+                }
+            }
+            isInSelectionChanged = false;
+        }
+
+        private void LoadList()
+        {
+            SelectList.Items.Clear();
+            foreach (var book in App.installedBibles.installedBibles)
+            {
+                TextBlock block = new TextBlock();
+                block.Text = book.Value.Name;
+                block.Tag = book.Value.sbmd.Initials;
+                block.TextWrapping = TextWrapping.Wrap;
+                SelectList.Items.Add(block); 
+            }
+        }
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            LoadList();
+        }
+    }
+}
