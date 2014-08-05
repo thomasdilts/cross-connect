630c38b25be04ed00dbbdd8392876b29e5ae223b
thomas.dilts60 <thomas.dilts60@gmail.com>
2014-08-05 09:05:11 +0000

    Files missing in SVN

---
+++ CrossConnect8.1/CrossConnectPhone/EasySlider.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CrossConnect
+{
+    public partial class EasySlider : UserControl
+    {
+        public double SliderValue
+        {
+            get
+            {
+                return this.sliderTextSize.Value;
+            }
+            set
+            {
+                sliderTextSize.Value = value;
+            }
+        }
+
+        public delegate void ValueChangedDelegate(object sender, RoutedPropertyChangedEventArgs<double> e);
+
+        public ValueChangedDelegate ValueChanged;
+
+        public EasySlider()
+        {
+            InitializeComponent();
+        }
+
+        private void ButMore_Click(object sender, RoutedEventArgs e)
+        {
+            sliderTextSize.Value = (int)sliderTextSize.Value;
+            if (sliderTextSize.Value < sliderTextSize.Maximum)
+            {
+                sliderTextSize.Value++;
+            }
+        }
+
+        private void ButLess_Click(object sender, RoutedEventArgs e)
+        {
+            sliderTextSize.Value = (int)sliderTextSize.Value;
+            if (sliderTextSize.Value > sliderTextSize.Minimum)
+            {
+                sliderTextSize.Value --;
+            }
+        }
+
+        private void SliderTextSizeValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (sliderTextSize != null)
+            {
+                sliderTextSize.Value = (int)sliderTextSize.Value;
+                this.SliderNumber.Text = sliderTextSize.Value.ToString();
+                if (ValueChanged != null)
+                {
+                    ValueChanged(sender, e);
+                }
+            }
+        }
+    }
+}
