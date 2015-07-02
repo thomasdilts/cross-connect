492ea3213a5cc7ef3bd0797706e44dbc9b5aa4d9
thomasdilts <thomas@cross-connect.se>
2015-07-02 15:41:04 +0200

    fixed 2 crashes with Dictionaries.

--- CrossConnect10/Sword/reader/DictionaryRawDefReader.cs
+++ CrossConnect10/Sword/reader/DictionaryRawDefReader.cs
@@ -223,7 +223,7 @@ namespace Sword.reader
         {
             var head = new StringBuilder();
             head.Append(
-                "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\">");
+                "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\" />");
 
             head.Append("<style>");
 
@@ -558,7 +558,7 @@ function SetFontColorForElement(elemntId, colorRgba){
             string chapterEndHtml = string.Empty;
             if (addStartFinishHtml)
             {
-                chapterStartHtml = HtmlHeader(
+                chapterStartHtml = DictionaryRawDefReader.HtmlHeader(
                     displaySettings,
                     htmlBackgroundColor,
                     htmlForegroundColor,
