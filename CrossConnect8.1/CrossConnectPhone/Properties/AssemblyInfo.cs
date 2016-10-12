51a35e157972982471e11c3534216a6b1c794bd4
thomasdilts <thomas@cross-connect.se>
2016-10-12 15:50:09 +0200

    revert bad commit

--- CrossConnect8.1/CrossConnectPhone/Properties/AssemblyInfo.cs
+++ CrossConnect8.1/CrossConnectPhone/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Revision
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.8.9")]
-[assembly: AssemblyFileVersion("2.0.8.9")]
+[assembly: AssemblyVersion("2.0.8.8")]
+[assembly: AssemblyFileVersion("2.0.8.8")]
 [assembly: NeutralResourcesLanguage("en")]
