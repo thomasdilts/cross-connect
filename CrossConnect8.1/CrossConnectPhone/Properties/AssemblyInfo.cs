ad17df22f24269463794645fc0991e0f87e40c99
thomasdilts <thomas@cross-connect.se>
2016-10-13 09:31:58 +0200

    More changes to Belarusian

--- CrossConnect8.1/CrossConnectPhone/Properties/AssemblyInfo.cs
+++ CrossConnect8.1/CrossConnectPhone/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Revision
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.9.0")]
-[assembly: AssemblyFileVersion("2.0.9.0")]
+[assembly: AssemblyVersion("2.0.9.1")]
+[assembly: AssemblyFileVersion("2.0.9.1")]
 [assembly: NeutralResourcesLanguage("en")]
